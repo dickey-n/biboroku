8626f3124e7897a1b46245f556ec995d00bd5d36
dickey-n <daiki.nose0501@gmail.com>
2020-10-28 10:54:56 +0900

    バリデーション定義

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -1,2 +1,13 @@
 class Museum < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :place
+    validates :text
+    validates :prefecture_id
+    validates :genre_id
+    validates :artist
+    validates :image
+  end
+
+  belongs_to :user
 end
