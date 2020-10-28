1bb53a2510f400f40755890feb07eafdae4c775c
dickey-n <daiki.nose0501@gmail.com>
2020-10-28 13:00:32 +0900

    編集、削除機能の実装

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -20,6 +20,28 @@ class MuseumsController < ApplicationController
     @museum = Museum.find(params[:id])
   end
 
+  def edit
+    @museum = Museum.find(params[:id])
+    unless current_user.id == @museum.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    museum = Museum.find(params[:id])
+    if museum.update(museum_params)
+      redirect_to museum_path(museum.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    museum = Museum.find(params[:id])
+    museum.destroy
+    redirect_to root_path
+  end
+
   private
   def museum_params
     params.require(:museum).permit(:title, :place, :prefecture_id, :genre_id, :artist, :text, :image).merge(user_id: current_user.id)
