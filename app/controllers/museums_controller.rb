80e33297abd3e936eb103e10f6fe7762bb6982ad
dickey-n <daiki.nose0501@gmail.com>
2020-10-28 11:51:27 +0900

    コントローラの編集

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -3,8 +3,20 @@ class MuseumsController < ApplicationController
   end
 
   def new
+    @museum = Museum.new
   end
 
   def create
+    @museum = Museum.new(museum_params)
+    if @museum.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:museum).permit(:title, :place, :prefecture_id, :genre_id, :artist, :text, :image).merge(user_id: current_user.id)
   end
 end
