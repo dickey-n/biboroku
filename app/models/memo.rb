4f1bb7d3c2f4489ab509915cb58ef7d2193a992f
dickey-n <daiki.nose0501@gmail.com>
2020-11-05 16:57:25 +0900

    ルーティング設定

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,9 +1,9 @@
 class Memo < ApplicationRecord
   belongs_to :user
   belongs_to :museum
-
-  validates :memo, presence: true
-
   has_many :likes
   has_many :liked_users, through: :likes, source: :user
+
+  validates :memo, presence: true
+  validates_uniqueness_of :memo_id, scope: :user_id
 end
