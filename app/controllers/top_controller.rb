45b8a88086c36ef4444f4fd3db2446b8269ecbfc
dickey-n <daiki.nose0501@gmail.com>
2020-11-05 11:20:32 +0900

    Moreの表示の条件設定

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,6 @@ class TopController < ApplicationController
   def index
     @all_museums = Museum.all
     @part_museums = Museum.order("created_at DESC").first(6)
-    @memos = Memo.order("created_at DESC").first(5)
+    @memos = Memo.order("created_at DESC").first(6)
   end
 end
