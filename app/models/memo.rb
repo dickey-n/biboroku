34c9d3a6b3b2e3a1c7e2837bba312352a801704f
dickey-n <daiki.nose0501@gmail.com>
2020-11-05 16:55:05 +0900

    アソシエーションの定義

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -3,4 +3,7 @@ class Memo < ApplicationRecord
   belongs_to :museum
 
   validates :memo, presence: true
+
+  has_many :likes
+  has_many :liked_users, through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,11 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :profile, presence: true
 
-  has_many :memos
   has_many :museums
+
+  has_many :memos, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_memos, through: :likes, source: :memo
 
   mount_uploader :image, ImageUploader
 end
