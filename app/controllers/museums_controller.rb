b5ad585bde361447d0be66714e65b0b89598f0c9
dickey-n <daiki.nose0501@gmail.com>
2020-11-05 14:17:03 +0900

    管理者用のコントローラー作成

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -5,48 +5,9 @@ class MuseumsController < ApplicationController
     @museums = Museum.all.order("created_at DESC")
   end
 
-  def new
-    @museum = Museum.new
-  end
-
-  def create
-    @museum = Museum.new(museum_params)
-    if @museum.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
   def show
     @museum = Museum.find(params[:id])
     @memos = @museum.memos.includes(:user)
   end
 
-  def edit
-    @museum = Museum.find(params[:id])
-    unless current_user.id == @museum.user_id
-      redirect_to action: :index
-    end
-  end
-
-  def update
-    museum = Museum.find(params[:id])
-    if museum.update(museum_params)
-      redirect_to museum_path(museum.id)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    museum = Museum.find(params[:id])
-    museum.destroy
-    redirect_to root_path
-  end
-
-  private
-  def museum_params
-    params.require(:museum).permit(:title, :place, :prefecture_id, :genre_id, :artist, :text, :image).merge(user_id: current_user.id)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'top#index'
 
-  resources :museums do
+  namespace :admin do
+    resources :museums, only: [:new, :create, :edit, :update, :destroy]
+  end
+
+  resources :museums, only: [:index, :show] do
     resources :memos, only: [:new, :index, :show, :create, :destroy]
   end
 
