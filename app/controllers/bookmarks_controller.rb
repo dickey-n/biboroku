54dcbbdaacbe635c22fae64f537fdf39db511f54
dickey-n <daiki.nose0501@gmail.com>
2020-11-06 14:37:47 +0900

    コントローラーの定義

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,22 @@
+class BookmarksController < ApplicationController
+  before_action :set_like
+  before_action :authenticate_user!
+
+  def create
+    @bookmark = Bookmark.create(bookmark_params)
+    @bookmark.save
+  end
+
+  def destroy
+    bookmark = Bookmark.find_by(user_id: current_user.id, museum_id: params[:id])
+    bookmark.destroy
+  end
+
+  def bookmark_params
+    params.permit(:museum_id, :user_id).merge(user_id: current_user.id, museum_id: params[:museum_id])
+  end
+
+  def set_like
+    @museum = Museum.find(params[:museum_id])
+  end
+end
