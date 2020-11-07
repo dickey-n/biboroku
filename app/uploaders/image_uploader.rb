66b81cb7a5cf97851da94e08e1527cc0df57ea59
dickey-n <daiki.nose0501@gmail.com>
2020-11-08 08:55:08 +0900

    carrierwaveの編集

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,44 +1,28 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  # リサイズしたり画像形式を変更するのに必要
   include CarrierWave::RMagick
 
-  # 画像の上限を640x480にする
   process resize_to_limit: [640, 480]
+  
+  def default_url(*_args)
+    ActionController::Base.helpers.asset_path('fallback/' + [version_name, 'default.png'].compact.join('_'))
+  end
 
-  # 保存形式をJPGにする
-  process convert: 'jpg'
-
-  storage :fog
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :aws
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # サムネイルを生成する設定
-  version :thumb do
-    process resize_to_limit: [300, 300]
+  def extension_whitelist
+    %w(jpg jpeg gif png)
   end
 
-  version :thumb100 do
-    process resize_to_limit: [100, 100]
-  end
-
-  version :thumb30 do
-    process resize_to_limit: [30, 30]
-  end
-
-  # jpg,jpeg,gif,pngしか受け付けない
-  def extension_white_list
-    %w[jpg jpeg gif png]
-  end
-
-  # 拡張子が同じでないとGIFをJPGとかにコンバートできないので、ファイル名を変更
-  def filename
-    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
-  end
-
-  # ファイル名を日付にするとタイミングのせいでサムネイル名がずれる
-  # ファイル名はランダムで一意になる
   def filename
     "#{secure_token}.#{file.extension}" if original_filename.present?
   end
@@ -46,15 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   protected
 
   def secure_token
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
-  end
-
-  # デフォルト画像の設定
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*_args)
-    #   For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.asset_path('fallback/' + [version_name, 'default.png'].compact.join('_'))
-    #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+     var = :"@#{mounted_as}_secure_token"
+     model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 end
