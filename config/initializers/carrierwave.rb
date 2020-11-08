ad6ae43f5e96414ac8306dad6bbb0a68bf05c69a
dickey-n <daiki.nose0501@gmail.com>
2020-11-08 09:25:38 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,13 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'biboroku'
-    config.asset_host = 'https://s3.amazonaws.com/biboroku'
-    config.fog_public = false
+    config.asset_host = 'https://biboroku.s3.amazonaws.com'
     config.fog_credentials = {
       provider: 'aws',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1',
-      #path_style: true
+      path_style: true
     }
   else
     config.storage :file
