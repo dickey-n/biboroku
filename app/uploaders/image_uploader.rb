3b0f8b0a2fcd1080009a1065bf1f62a83f1257be
dickey-n <daiki.nose0501@gmail.com>
2020-11-16 11:22:32 +0900

    CarrierWaveの修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,6 +15,12 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :fog
   end
 
+  if Rails.env.development? || Rails.env.test? 
+    storage :file
+  else
+    storage :fog
+  end
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
